3654bff2ad971803ebbba1b07f8615d1b05051f8
Vikramaditya Singh <59855919+Ghat0tkach@users.noreply.github.com>
2023-10-26 13:45:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,10 @@ const HomePage = () => {
 
   return (
     <>
+      <div className="mx-auto  max-w-2xl px-6">
      <Header/>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">{postPreviews}</div>
+        </div>
     </>
   
   );
