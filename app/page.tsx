5cd76f26a7da406c47c6c24171fe278f18e3a7b1
Vikramaditya Singh <59855919+Ghat0tkach@users.noreply.github.com>
2023-10-26 14:08:11 +0530

    feat:fixed build error

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ function LandingPage() {
     <>
     <Background/>
   
-       <Link to="/home"> {/* Use Link for navigation */}
+       <Link href="/home"> {/* Use Link for navigation */}
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full sm:py-3 sm:px-6 lg:py-4 lg:px-8">
        explore
       </button>
