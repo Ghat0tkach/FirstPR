2b4728329c6ce0691324c69440e6df5aec424db9
Vikramaditya Singh <59855919+Ghat0tkach@users.noreply.github.com>
2023-10-26 13:59:57 +0530

    Update page.jsx

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Background from '../../components/HomeScreenBackground/Background'
 
 
-function LandingPage() {
+
+
+const HomePage = () => {
+  const postMetadata = getPostMetadata();
+  const postPreviews = postMetadata.map((post) => (
+    <PostPreview key={post.slug} {...post} />
+  ));
+
   return (
     <>
-    <Background/>
-    {/* <ButtonLayout/> */}
+      <div className="mx-auto  max-w-2xl px-6">
+     <Header/>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">{postPreviews}</div>
+        </div>
     </>
-  )
-}
-
-export default LandingPage
+  
+  );
+};
