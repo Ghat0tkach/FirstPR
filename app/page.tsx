83e6bf9e212483f0c2443fce73911764ab0a2cb5
Vikramaditya Singh <59855919+Ghat0tkach@users.noreply.github.com>
2023-10-26 14:01:21 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,19 @@
-import getPostMetadata from "../components/utils/getPostMetadata";
-import PostPreview from "../components/utils/PostPreview";
-import Header from "../components/header/header"
-const HomePage = () => {
-  const postMetadata = getPostMetadata();
-  const postPreviews = postMetadata.map((post) => (
-    <PostPreview key={post.slug} {...post} />
-  ));
+import React from 'react'
+import Background from '../../components/HomeScreenBackground/Background'
 
+
+function LandingPage() {
   return (
     <>
-      <div className="mx-auto  max-w-2xl px-6">
-     <Header/>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">{postPreviews}</div>
-        </div>
-    </>
+    <Background/>
   
-  );
-};
+       <Link to="/home"> {/* Use Link for navigation */}
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full sm:py-3 sm:px-6 lg:py-4 lg:px-8">
+       explore
+      </button>
+    </Link>
+    </>
+  )
+}
 
-export default HomePage;
+export default LandingPage
